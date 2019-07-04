6c0f683aa092723f2dadfdd2bbc01163982f5d06
Bryce Morgan <bamorgan13@gmail.com>
2019-07-04 11:48:22 -0400

    Create StaticObject class to encapsulate MovingObject in order to accommodate future explosion and powerup objects

--- bundle.js
+++ bundle.js
@@ -394,53 +394,15 @@ module.exports = Game;
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-const Util = __webpack_require__(/*! ./util */ "./lib/util.js");
+const StaticObject = __webpack_require__(/*! ./static_object */ "./lib/static_object.js");
 
-class MovingObject {
-	constructor({
-		pos,
-		vel,
-		path,
-		deceleration = 1,
-		scale = 1,
-		spriteWidth,
-		spriteHeight,
-		game,
-		speedMultiplier = 1,
-		spriteMaxX = 1,
-		spriteMaxY = 1,
-		direction
-	}) {
-		this.deceleration = deceleration;
-		this.direction = direction;
-		this.pos = pos;
-		this.vel = vel;
-		this.spriteWidth = spriteWidth;
-		this.spriteHeight = spriteHeight;
-		this.speedMultiplier = speedMultiplier;
-		this.img = new Image();
-		this.img.src = path;
-		this.spriteIndex = { x: 0, y: 0, maxX: spriteMaxX, maxY: spriteMaxY };
-		this.scale = scale;
-		this.width = this.spriteWidth * this.scale;
-		this.height = this.spriteHeight * this.scale;
-		this.game = game;
-	}
-
-	draw(ctx) {
-		if (this.img.naturalWidth > 0) {
-			ctx.drawImage(
-				this.img,
-				0 + this.spriteIndex.x * this.spriteWidth,
-				0 + this.spriteIndex.y * this.spriteHeight,
-				this.spriteWidth,
-				this.spriteHeight,
-				this.pos[0],
-				this.pos[1],
-				this.width,
-				this.height
-			);
-		}
+class MovingObject extends StaticObject {
+	constructor(options) {
+		super(options);
+		this.deceleration = options.deceleration || 1;
+		this.direction = options.direction;
+		this.vel = options.vel;
+		this.speedMultiplier = options.speedMultiplier || 1;
 	}
 
 	move() {
@@ -450,37 +412,7 @@ class MovingObject {
 		this.vel[0] *= this.deceleration;
 		this.vel[1] *= this.deceleration;
 
-		if (this.game.frameIndex % 10 === 0) {
-			this.spriteIndex.x++;
-			this.spriteIndex.x = this.spriteIndex.x % this.spriteIndex.maxX;
-			this.spriteIndex.y++;
-			this.spriteIndex.y = this.spriteIndex.y % this.spriteIndex.maxY;
-		}
-
 		if (this.pos[0] < -1 * this.width || this.pos[0] > this.game.gameCanvas.width) this.remove();
-	}
-
-	xBounds() {
-		return [this.pos[0], this.pos[0] + this.width];
-	}
-
-	yBounds() {
-		return [this.pos[1], this.pos[1] + this.height];
-	}
-
-	isCollidedWith(otherObject) {
-		return !(
-			this.xBounds()[0] > otherObject.xBounds()[1] ||
-			this.xBounds()[1] < otherObject.xBounds()[0] ||
-			this.yBounds()[0] > otherObject.yBounds()[1] ||
-			this.yBounds()[1] < otherObject.yBounds()[0]
-		);
-	}
-
-	collideWith(otherObject) {}
-
-	remove() {
-		this.game.remove(this);
 	}
 }
 
@@ -612,6 +544,80 @@ class Ship extends MovingObject {
 }
 
 module.exports = Ship;
+
+
+/***/ }),
+
+/***/ "./lib/static_object.js":
+/*!******************************!*\
+  !*** ./lib/static_object.js ***!
+  \******************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+class StaticObject {
+	constructor({ pos, path, scale = 1, spriteWidth, spriteHeight, game, spriteMaxX = 1, spriteMaxY = 1 }) {
+		this.pos = pos;
+		this.spriteWidth = spriteWidth;
+		this.spriteHeight = spriteHeight;
+		this.img = new Image();
+		this.img.src = path;
+		this.spriteIndex = { x: 0, y: 0, maxX: spriteMaxX, maxY: spriteMaxY };
+		this.scale = scale;
+		this.width = this.spriteWidth * this.scale;
+		this.height = this.spriteHeight * this.scale;
+		this.game = game;
+		this.framesDrawn = 0;
+	}
+
+	draw(ctx) {
+		if (this.img.naturalWidth > 0) {
+			ctx.drawImage(
+				this.img,
+				0 + this.spriteIndex.x * this.spriteWidth,
+				0 + this.spriteIndex.y * this.spriteHeight,
+				this.spriteWidth,
+				this.spriteHeight,
+				this.pos[0],
+				this.pos[1],
+				this.width,
+				this.height
+			);
+
+			if (this.framesDrawn % 10 === 0) {
+				this.spriteIndex.x++;
+				this.spriteIndex.x = this.spriteIndex.x % this.spriteIndex.maxX;
+				this.spriteIndex.y++;
+				this.spriteIndex.y = this.spriteIndex.y % this.spriteIndex.maxY;
+			}
+
+			this.framesDrawn++;
+		}
+	}
+
+	xBounds() {
+		return [this.pos[0], this.pos[0] + this.width];
+	}
+
+	yBounds() {
+		return [this.pos[1], this.pos[1] + this.height];
+	}
+
+	isCollidedWith(otherObject) {
+		return !(
+			this.xBounds()[0] > otherObject.xBounds()[1] ||
+			this.xBounds()[1] < otherObject.xBounds()[0] ||
+			this.yBounds()[0] > otherObject.yBounds()[1] ||
+			this.yBounds()[1] < otherObject.yBounds()[0]
+		);
+	}
+
+	remove() {
+		this.game.remove(this);
+	}
+}
+
+module.exports = StaticObject;
 
 
 /***/ }),
